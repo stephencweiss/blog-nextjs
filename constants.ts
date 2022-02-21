564ed7f24959f4e3a62f0c9d5371f476aa31921f
Stephen <stephen.c.weiss@gmail.com>
2022-02-21 17:07:03 -0600

    🐛 fix NOTES_PATH for builds

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@ import path from "path";
 /**
  * The full path to the directory where the blog content lives
  */
-export const NOTES_PATH = path.join(__dirname, "../../../", "content/notes");
+export const NOTES_PATH = path.join(process.cwd(), "content/notes");
