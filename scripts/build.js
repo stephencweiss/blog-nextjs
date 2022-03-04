d99bbc97159889f5d0bae5bbf5d73affe31045fc
Stephen <stephen.c.weiss@gmail.com>
2022-03-04 17:05:19 -0600

    🧰 Add search index to dictionaries script
    The search index is not "technically" a dictionary but a list of
    all of the posts that will hopefully be in a format that is
    searchable. Or that's the hope at a minimum :)

--- scripts/build.js
+++ scripts/build.js
@@ -12,45 +12,76 @@ const fileFilter = (parentDir, fileName) => {
 const filePath = (fileName) => path.join(NOTES_PATH, fileName);
 
 function extractFrontmatter(fileName) {
-  const content = fs.readFileSync(filePath(fileName), "utf-8");
-  const { data: frontmatter } = matter(content);
+  const fileData = fs.readFileSync(filePath(fileName), "utf-8");
+  const {
+    data: { private: privateKey, ...frontmatter },
+    content,
+  } = matter(fileData);
 
   const slug = createSlug(fileName, frontmatter);
+  const isPublished = String(frontmatter?.stage)
+    .toLowerCase()
+    .includes("publish");
 
-  return { ...frontmatter, slug, fileName };
+  return {
+    ...frontmatter,
+    content,
+    slug,
+    fileName,
+    isPrivate: privateKey || frontmatter.archived || !isPublished,
+  };
 }
 
 function createSlug(fileName, frontmatter) {
   return frontmatter?.slug ?? fileName.replace(".md", "");
 }
 
-function buildDictionary() {
+function buildDictionary(dictionary, dictionaryName) {
+  const dirPath = path.join(process.cwd(), "dictionaries");
+  try {
+    fs.readdirSync(dirPath);
+  } catch (e) {
+    fs.mkdirSync(dirPath);
+  }
+
+  console.log(dictionaryName, Array.isArray(dictionary));
+
+  const data = Array.isArray(dictionary)
+    ? JSON.stringify(dictionary)
+    : JSON.stringify([...dictionary.entries()]);
+
+  fs.writeFileSync(`${dirPath}/${dictionaryName}.json`, data, {
+    encoding: "utf-8",
+  });
+}
+
+function buildDictionaries() {
   const slugDictionary = new Map();
   const fileNameDictionary = new Map();
+  const searchDictionary = [];
 
   const dir = fs.readdirSync(NOTES_PATH);
 
   const files = dir.filter((fileName) => fileFilter(NOTES_PATH, fileName));
   const extracted = files.map(extractFrontmatter);
-  extracted.forEach(({ fileName, stage, private = false, slug }) => {
-    slugDictionary.set(slug, { fileName, stage, slug, private });
-    fileNameDictionary.set(fileName, { fileName, stage, slug, private });
-  });
+  extracted.forEach(
+    ({ fileName, stage, isPrivate = false, slug, content, ...rest }) => {
+      slugDictionary.set(slug, { fileName, stage, slug, isPrivate });
+      fileNameDictionary.set(fileName, { fileName, stage, slug, isPrivate });
+      searchDictionary.push({
+        fileName,
+        stage,
+        slug,
+        isPrivate,
+        content,
+        ...rest,
+      });
+    }
+  );
 
-  fs.writeFileSync(
-    path.join(__dirname, "../dictionaries/slugDictionary.json"),
-    JSON.stringify([...slugDictionary.entries()]),
-    {
-      encoding: "utf-8",
-    }
-  );
-  fs.writeFileSync(
-    path.join(__dirname, "../dictionaries/fileNameDictionary.json"),
-    JSON.stringify([...fileNameDictionary.entries()]),
-    {
-      encoding: "utf-8",
-    }
-  );
+  buildDictionary(slugDictionary, "slugDictionary");
+  buildDictionary(fileNameDictionary, "fileNameDictionary");
+  buildDictionary(searchDictionary, "searchDictionary");
 }
 
-buildDictionary();
+buildDictionaries();
