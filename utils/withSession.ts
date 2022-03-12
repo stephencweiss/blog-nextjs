6519d0631cfe842d0439c0ec82a820fc7e47628d
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-03-12 12:10:34 -0600

    🐛 Catch missing cookies / .env files

--- utils/withSession.ts
+++ utils/withSession.ts
@@ -1,9 +1,12 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import type { IronSessionOptions } from "iron-session";
-import type { User } from "../pages/api/user";
+import type { User } from "@/pages/api/user";
+
+if (!process.env.SECRET_COOKIE_PASSWORD)
+  throw new Error("Missing Cookie Password env variable");
 
 export const sessionOptions: IronSessionOptions = {
-  password: { 1: process.env.SECRET_COOKIE_PASSWORD as string },
+  password: { 1: process.env.SECRET_COOKIE_PASSWORD },
   cookieName: process.env.IRON_SESSION_COOKIE_NAME ?? "iron-session-cookie",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
