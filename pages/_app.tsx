6b4c503acfa06c7143a61853b607038a0f556276
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-03-23 10:48:01 -0600

    💅 remove unused style sheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/reset.scss";
 import "../styles/globals.scss";
 import "../styles/theme.scss";
-import "../styles/theme-application.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
