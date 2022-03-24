5d5698e6ed7c8ba249ed331f1cf0571a8ccc5200
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-03-24 11:46:50 -0600

    💅 wip - styling dynamic post

--- pages/blog/[post].tsx
+++ pages/blog/[post].tsx
@@ -17,18 +17,44 @@ import {
 } from "utils";
 import { extractNoteData } from "../../utils/serverUtils";
 import { NavBar } from "../../components/NavBar";
+import { formatDate } from "utils/formatters";
+import { useEffect, useState } from "react";
 
 const dict: Dictionary = rebuildDictionary(dictionary);
 const PostPage: NextPage<ExpandedNote> = (props) => {
-  const { content, title, date } = props;
+  const { content, title, date, updated, publish } = props;
+  const [postDate, setPostDate] = useState<string>();
+  const [updatedDate, setUpdatedDate] = useState<string>();
+
+  useEffect(() => {
+    try {
+      setPostDate(formatDate(publish ?? date));
+    } catch (e) {}
+  }, [date, publish]);
+
+  useEffect(() => {
+    try {
+      const latest = Array.isArray(updated)
+        ? updated[updated.length - 1]
+        : updated;
+      latest && setUpdatedDate(formatDate(latest));
+    } catch (e) {}
+  }, [updated]);
+  // todo: format date
+  // add categories
+  // add tags
+  // style code
+  // add backlinks
+
   return (
     <>
       <NavBar />
       <div>
-        <h1>{title}</h1>
-        <div>Posted on {date}</div>
-        <div>
-          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        <h1 className="text1 text-4xl font-bold">{title}</h1>
+        {postDate ? <div>Posted on {postDate}</div> : <></>}
+        {updatedDate ? <div>Last updated on {updatedDate}</div> : <></>}
+        <div className="post-body">
+          <div dangerouslySetInnerHTML={{ __html: content }}></div>
         </div>
       </div>
     </>
@@ -49,13 +75,15 @@ async function wrappableServerSideProps(
 
   const specific = dict.get(post) ?? ({} as PostLookup);
   const note = extractNoteData(specific.fileName);
-
   // if the post is private and the user isn't an admin, show 404
   if (note?.isPrivate && user?.admin !== true) {
     return { notFound: true };
   }
+  const { content } = note;
+  const html = marked.parse(content);
+  console.log(JSON.stringify({ content, html }, null, 4));
 
-  return { props: { ...note } };
+  return { props: { ...note, content: html } };
 }
 
 export default PostPage;
