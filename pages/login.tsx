ebb0e129561ddc64da7aeb3371aba45273ed9ad7
Stephen Weiss <sweiss@plaid.com>
2022-04-27 08:50:49 -0500

    ✨ Make login page more dynamic

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,16 @@
+import { withIronSessionSsr } from "iron-session/next";
+import {
+  GetServerSidePropsResult,
+  GetServerSidePropsContext,
+  PreviewData,
+} from "next";
+import { NextParsedUrlQuery } from "next/dist/server/request-meta";
 import { useRouter } from "next/router";
 import * as React from "react";
-import { extractFormKeyValue } from "utils";
+import { User } from "types/index";
+import { extractFormKeyValue, sessionOptions } from "utils";
 
-export type Props = {};
+export type Props = User & {};
 
 function resStatusIsOkay(res: Response) {
   return res.status >= 200 && res.status < 400;
@@ -28,19 +36,44 @@ function Login(props: Props) {
 
   const onLogout = async () => {
     await fetch("/api/logout");
+    router.push("/");
   };
   return (
     <div>
-      <button onClick={onLogout}>Logout</button>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Password
-          <input type="password" name="password" />
-        </label>
-        <button>Submit</button>
-      </form>
+      <h1 className="text1 text-2xl">Login</h1>
+      <p className="text1">
+        Please note: At this time, there is no means to sign up for an account
+        on this site. The login feature allows me to view notes in progress,
+        edit existing notes, and write new notes from the web (feature coming).
+      </p>
+      {!props.isLoggedIn ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Password
+            <input type="password" name="password" />
+          </label>
+          <button>Submit</button>
+        </form>
+      ) : (
+        <button onClick={onLogout}>Logout</button>
+      )}
     </div>
   );
 }
 
 export default Login;
+
+export const getServerSideProps = withIronSessionSsr(
+  wrappableServerSideProps,
+  sessionOptions
+);
+
+function wrappableServerSideProps(
+  context: GetServerSidePropsContext<NextParsedUrlQuery, PreviewData>
+): GetServerSidePropsResult<User> {
+  const { req } = context;
+
+  const user = req?.session?.user || ({} as User);
+
+  return { props: { ...user } };
+}
