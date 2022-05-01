bc47f82cc4476b0403a051c4637b738fd7079d17
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-05-01 15:43:14 -0500

    remove css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/theme.scss";
 import "../styles/reset.scss";
 import "../styles/globals.scss";
-import "prismjs/themes/prism-solarizedlight.min.css";
-import "prismjs/plugins/autolinker/prism-autolinker.min.css";
-import "prismjs/plugins/command-line/prism-command-line.min.css";
-import "prismjs/plugins/diff-highlight/prism-diff-highlight.min.css";
-import "prismjs/plugins/inline-color/prism-inline-color.min.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.min.css";
+// import "prismjs/themes/prism-solarizedlight.min.css";
+// import "prismjs/plugins/autolinker/prism-autolinker.min.css";
+// import "prismjs/plugins/command-line/prism-command-line.min.css";
+// import "prismjs/plugins/diff-highlight/prism-diff-highlight.min.css";
+// import "prismjs/plugins/inline-color/prism-inline-color.min.css";
+// import "prismjs/plugins/line-numbers/prism-line-numbers.min.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
