791e7481c7e7ad3651c03061938d23b10e040313
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-05-01 17:48:45 -0500

    trying to parse my own markdown and convert it into md

--- utils/serverUtils/markdownToHtml.ts
+++ utils/serverUtils/markdownToHtml.ts
@@ -1,9 +1,21 @@
+import { unified } from "unified";
+import mdParse from "remark-parse";
 // import { remark } from "remark";
 // import html from "remark-html";
-// import prism from "remark-prism";
+import prism from "remark-prism";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import remarkStringify from "remark-stringify";
 
-export async function markdownToHtml(markdown: string) {
-  return markdown;
+export async function markdownToHtml(content: string) {
+  return await unified()
+    .use(mdParse)
+    .use(prism)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(remarkStringify)
+    .process(content);
+  // const result = engine.parse(content)
 
   // const result = await remark()
   //   // https://github.com/sergioramos/remark-prism/issues/265
