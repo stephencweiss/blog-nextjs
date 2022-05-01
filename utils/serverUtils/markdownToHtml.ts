bf70bd65de7cfdd3fccc4944c16d03639615d4db
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-05-01 17:21:15 -0500

    what happens if i don't invoke remark?

--- utils/serverUtils/markdownToHtml.ts
+++ utils/serverUtils/markdownToHtml.ts
@@ -1,12 +1,14 @@
-import { remark } from "remark";
-import html from "remark-html";
-import prism from "remark-prism";
+// import { remark } from "remark";
+// import html from "remark-html";
+// import prism from "remark-prism";
 
 export async function markdownToHtml(markdown: string) {
-  const result = await remark()
-    // https://github.com/sergioramos/remark-prism/issues/265
-    .use(html, { sanitize: false })
-    .use(prism)
-    .process(markdown);
-  return result.toString();
+  return markdown;
+
+  // const result = await remark()
+  //   // https://github.com/sergioramos/remark-prism/issues/265
+  //   .use(html, { sanitize: false })
+  //   .use(prism)
+  //   .process(markdown);
+  // return result.toString();
 }
