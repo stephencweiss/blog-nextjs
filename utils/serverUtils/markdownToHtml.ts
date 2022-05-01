d24b05a580e802e20b45c077db4c54db233f93ed
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-05-01 15:38:58 -0500

    remove plugins

--- utils/serverUtils/markdownToHtml.ts
+++ utils/serverUtils/markdownToHtml.ts
@@ -6,19 +6,7 @@ export async function markdownToHtml(markdown: string) {
   const result = await remark()
     // https://github.com/sergioramos/remark-prism/issues/265
     .use(html, { sanitize: false })
-    .use(prism, {
-      plugins: [
-        "autolinker",
-        "command-line",
-        "data-uri-highlight",
-        "diff-highlight",
-        "inline-color",
-        "keep-markup",
-        "line-numbers",
-        "show-invisibles",
-        "treeview",
-      ],
-    })
+    .use(prism)
     .process(markdown);
   return result.toString();
 }
